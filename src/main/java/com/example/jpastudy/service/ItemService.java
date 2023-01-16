29b1a9340b451dab64ce88889d089b36d3af8569
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2023-01-16 12:13:53 +0900

    Item Service Develop

--- src/main/java/com/example/jpastudy/service/ItemService.java
+++ src/main/java/com/example/jpastudy/service/ItemService.java
@@ -1,2 +1,31 @@
-package com.example.jpastudy.service;public class ItemService {
+package com.example.jpastudy.service;
+
+import com.example.jpastudy.domain.item.Item;
+import com.example.jpastudy.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItem(Item item) {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long id) {
+        return itemRepository.findOne(id);
+    }
+
 }
