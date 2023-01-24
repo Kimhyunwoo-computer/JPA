d46fb94bbd9d8acc9fa0e03444172e14321260e3
Kimhyunwoo-computer <rlagusdn3046@naver.com>
2023-01-25 00:58:22 +0900

    CreateMemeberApi

--- src/main/java/com/example/jpastudy/service/MemberService.java
+++ src/main/java/com/example/jpastudy/service/MemberService.java
@@ -50,4 +50,10 @@ public class MemberService {
         return memberRepository.findOne(memberId);
     }
 
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = new Member();
+        member.setName(name);
+
+    }
 }
